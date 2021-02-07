f369bbf8ba8fdf9a0579c71e3cb96809073bfb13
Dan Ranges <danranges@gmail.com>
2021-02-07 17:46:57 +0100

    Add basic game functionality

--- ttt.js
+++ ttt.js
@@ -1,17 +1,56 @@
 
-// winning move combos
-// 012, 345, 678, 036, 147, 258, 048, 246
+let playerCount
+let turn = 0
+
+
+
+let gameSetup = (function() {
+    const onePlayer = document.getElementById('btn-one-player')
+    const twoPlayer = document.getElementById('btn-two-player')
+    const playerOneName = document.getElementById('player-one-name')
+    const playerTwoName = document.getElementById('player-two-name')
+    const playerTwoContainer = document.getElementById('player-two-container')
+    const btnSubmit = document.getElementById('btn-submit-names')
+    const playerNamesContainer = document.getElementById('player-names-container')
+    const playerCountContainer = document.getElementById('players-container')
+    
+    onePlayer.addEventListener('click', onePlayerGame)
+    twoPlayer.addEventListener('click', twoPlayerGame)
+    btnSubmit.addEventListener('click', submitNames)
+
+    function onePlayerGame() {
+        playerCount = 1
+        chooseNames()
+    }
+    function twoPlayerGame() {
+        playerCount = 2
+        chooseNames()
+    }
+
+    function chooseNames() {
+        playerCountContainer.style.visibility = 'hidden'
+        playerNamesContainer.style.visibility = 'visible'
+
+        if (playerCount === 1) {
+            playerTwoContainer.style.visibility = 'hidden'
+        }
+    }
+
+    function submitNames() {
+        // TODO
+    }
+
+})()
 
 let gameBoard  = (function() {
     board = []
-    let players
-    let turn = 0
+    let gameOver = false
     
     const container = document.getElementById('game-board-container')
     const status = document.getElementById('status')
-    const btnReset = document.getElementById('reset')
+    const btnReset = document.getElementById('new-game')
 
-    btnReset.addEventListener('click', _reset)
+    btnReset.addEventListener('click', _newGame)
 
     _render()
 
@@ -30,13 +69,44 @@ let gameBoard  = (function() {
         container.innerHTML = ''
     }
 
-    function _winCheck() {
+    function _winCheck(marker) {
+        let tie = false
+        // winning move combos
+        // 012, 345, 678, 036, 147, 258, 048, 246
+        if ((marker === board[0] && marker === board[1] && marker === board[2]) || 
+            (marker === board[3] && marker === board[4] && marker === board[5]) ||
+            (marker === board[6] && marker === board[7] && marker === board[8]) ||
+            (marker === board[0] && marker === board[3] && marker === board[6]) ||
+            (marker === board[1] && marker === board[4] && marker === board[7]) ||
+            (marker === board[2] && marker === board[5] && marker === board[8]) ||
+            (marker === board[0] && marker === board[4] && marker === board[8]) ||
+            (marker === board[2] && marker === board[4] && marker === board[6])) {
+                gameOver = true
+            } else if (board[0] && board[1] && board[2] && board[3] && board[4] && 
+                       board[5] && board[6] && board[7] && board[8]) {
+                gameOver = true
+                tie = true
+            }
 
+        if (gameOver) {
+            if (tie) {
+                status.textContent = 'It\'s a draw'
+            } else {
+                if (marker === 'X') {
+                    status.textContent = 'Player One wins!'
+                } else {
+                    status.textContent = 'Player Two wins!'
+                }
+            }
+        }
     }
     
-    function _reset() {
+    function _newGame() {
         board = []
+        gameOver = false
+        tie = false
         _destroy()
+        _render()
     }
 
     function _addMove(space, marker) {
@@ -44,28 +114,31 @@ let gameBoard  = (function() {
             board[space] = marker
             _destroy()
             _render()
-            _winCheck()
+            _winCheck(marker)
         }
     }
-
+    
     function playerMove(i) {
         if (turn === 0) {
             _addMove(i, 'X')
         } else {
             _addMove(i, 'O')
         }
+    
+        if (!turn && !gameOver) {
+            turn = 1
+            if (playerCount === 1) {
+               setTimeout(() => {computer.simMove(board)}, 500)
+            }
+        } else {
+            turn = 0
+        }
     }
-    
-    
-
 
     return {
-        addMove,
-        players,
+        playerMove,
         reset
     }
-    
-    
 })()
 
 let computer = (function() {
@@ -81,10 +154,14 @@ let computer = (function() {
         } 
     }
 
-    
     return {
         simMove
     }
 
 })()
+
+function player(name, marker){
+    this.name = name
+    this.marker = marker
+}
 
